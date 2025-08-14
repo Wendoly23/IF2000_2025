7eba2a7aea830cc46eb9e4f80a0cfadded37c52e
Wendoly Cruz <63074400cruz@gmail.com>
2025-08-14 11:44:37 -0600

    Commit del profe

---
+++ IF2000_IntroduccionProgra/src/if2000_introduccionprogra/IF2000_IntroduccionProgra.java
@@ -0,0 +1,44 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package if2000_introduccionprogra;
+
+/**
+ *
+ * @author Hp
+ */
+public class IF2000_IntroduccionProgra {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        String name = "Lionel";
+        int age = 40;
+        
+        /*Tipos de datos numericos:
+        int, long, short, float, double, numeric
+        money, decimal
+        
+        Manejo de fechas:
+        Date
+        
+        cadena de caracteres:
+        String
+        
+        objetos:
+        
+        object
+        
+        Data Structures: Array or Vectors: int[] x:
+        int [][] y:
+        */
+        
+        System.out.println( "Hello World");
+        System.out.println("Name:" + name);
+        System.out.println("Age:" + age);
+    }
+    
+}
